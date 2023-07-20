74796864f8b85c42df55fd701a24d33b48b897f6
“Hafiz” <hafizzudinjafri326@gmail.com>
2023-07-20 21:49:28 +0545

    main menu onclick animation

--- lib/feature/layout/main_menu_view.dart
+++ lib/feature/layout/main_menu_view.dart
@@ -14,7 +14,7 @@ class MainMenuView extends StatefulWidget {
   State<MainMenuView> createState() => _MainMenuViewState();
 }
 
-class _MainMenuViewState extends State<MainMenuView> {
+class _MainMenuViewState extends State<MainMenuView> with SingleTickerProviderStateMixin{
 
   int pageViewIndex = 0;
   List menuItems = [
@@ -45,6 +45,7 @@ class _MainMenuViewState extends State<MainMenuView> {
   ];
 
   late PageController pageController;
+  late AnimationController _animationController;
 
 
   @override
@@ -52,6 +53,10 @@ class _MainMenuViewState extends State<MainMenuView> {
     super.initState();
     pageController = PageController(initialPage: pageViewIndex, viewportFraction: 0.3);
 
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 250),
+      vsync: this,
+    );
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       setState(() {});
     });
@@ -59,8 +64,9 @@ class _MainMenuViewState extends State<MainMenuView> {
 
   @override
   void dispose() {
-    super.dispose();
+    _animationController.dispose();
     pageController.dispose();
+    super.dispose();
   }
 
   @override
@@ -127,43 +133,78 @@ class _MainMenuViewState extends State<MainMenuView> {
     );
   }
 
+
+  int tappedMainMenuIndex = 0;
+  Color tappedMainMenuColor = Colors.white;
+  Color tappedMainMenuContentColor = Colors.black;
   _cards(int index) {
     LayoutViewModel layoutViewModel = context.read<LayoutViewModel>();
-    return InkWell(
-      splashColor: primaryButtonColor,
-      highlightColor: primaryButtonColor,
+    return GestureDetector(
       onTap: () {
         layoutViewModel.onClickMainMenu(index, context);
       },
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(8)
-        ),
-        alignment: Alignment.center,
-        margin: const EdgeInsets.symmetric(horizontal: 4, vertical: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 64,
-              height: 64,
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: gray200Color, width: 1.5),
+      onPanDown: (_) {
+        setState(() {
+          tappedMainMenuIndex = index;
+          tappedMainMenuColor = primaryButtonColor;
+          tappedMainMenuContentColor = Colors.white;
+        });
+        _animationController.forward();
+      },
+      onPanCancel: () {
+        setState(() {
+          tappedMainMenuColor = Colors.white;
+          tappedMainMenuContentColor = Colors.black;
+        });
+        _animationController.reverse();
+      },
+      child: ScaleTransition(
+        scale: Tween<double>(
+            begin: 1.0,
+            end: index == tappedMainMenuIndex
+                ? 0.95
+                : 1
+        ).animate(_animationController),
+        child: Container(
+          decoration: BoxDecoration(
+            color: index == tappedMainMenuIndex
+                ? tappedMainMenuColor
+                : Colors.white,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          alignment: Alignment.center,
+          margin: const EdgeInsets.symmetric(horizontal: 4, vertical: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 64,
+                height: 64,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: gray200Color, width: 1.5),
+                ),
+                child: SVGImage(
+                  assetPath: menuItems[index]["icon"],
+                  color: index == tappedMainMenuIndex
+                      ? tappedMainMenuContentColor
+                      : Colors.black,
+                ),
               ),
-              child: SVGImage(
-                assetPath: menuItems[index]["icon"],
+              const SizedBox(height: 12),
+              Text(
+                menuItems[index]["title"],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: index == tappedMainMenuIndex
+                        ? tappedMainMenuContentColor
+                        : Colors.black,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600),
               ),
-            ),
-            const SizedBox(height: 12),
-            Text(
-              menuItems[index]["title"],
-              textAlign: TextAlign.center,
-              style: const TextStyle(color: Colors.black, fontSize: 12, fontWeight: FontWeight.w600),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
